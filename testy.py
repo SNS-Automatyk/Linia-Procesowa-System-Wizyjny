bb3cb4950ff98661e15faee7911b069e4e471f62
Marvin Ruciński <marvin@prasa-polska.com>
2025-07-11 15:22:53 +0100

    Initial commit (old software)

---
+++ testy.py
@@ -0,0 +1,16 @@
+import cv2 as cv
+import numpy as np
+from math import floor
+cam = cv.VideoCapture("/dev/video0")
+
+
+if not cam.isOpened():
+    print("Cannot open camera")
+    exit()
+
+
+ret, frame = cam.read()
+ 
+cv.imshow("Obraz z kamery", frame)
+
+
